416912fa2f7dc73014c008d0bf7a039599b630df
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2024-08-30 11:31:40 +0500

    init

--- src/js/app.js
+++ src/js/app.js
@@ -159,3 +159,74 @@ if (clicked) {
 }, false);
 window.addEventListener('touchend', mouseup, false);
 window.addEventListener('mouseup', mouseup, false);
+
+
+
+import barba from '@barba/core';
+
+function pageTransition() {
+  var tl = gsap.timeline();
+  tl.to('ul.transition li', {duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
+  tl.to('ul.transition li', {duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1})
+}
+
+function contentAnimation(){
+  var tl = gsap.timeline();
+  tl.from(".intro__title .line span", 1, {y: 120, ease: "power1.out", delay: 1, skewY: 17, stagger: {amount: 0}}, "-=1.1")
+  tl.from('.intro__desc', {duration: .8, translateY: 10, opacity: 0})
+  tl.from('.intro__btn', {duration: .8, translateY: 10, opacity: 0})
+  tl.to('.intro__img', {clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)"}, "-=1.1") 
+}
+
+function setMenuLinks() {
+  const links = document.querySelectorAll('.header__link');
+  const location = window.location.pathname;
+  links.forEach(link => {
+      const linkHref = '/' + link.getAttribute('href');
+      if(linkHref == location){
+          link.classList.add('active');
+          link.setAttribute('href', '#!');
+      }else{
+          link.classList.remove('active');
+          const dataHref = link.getAttribute('data-href');
+          link.setAttribute('href', dataHref);
+      }
+  });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+      setMenuLinks();
+  }, 200);
+})
+
+
+
+function delay(n) {
+  n = n || 2000;
+  return new Promise(done => {
+      setTimeout(() => {
+          done();
+      }, n);
+  });
+}
+
+
+barba.init({
+  sync: true,
+  transitions: [{
+    async leave(data) {
+      const done = this.async();
+      pageTransition();
+      setMenuLinks();
+      await delay(1500);
+      done();
+    },
+    async enter(data) {
+      contentAnimation();
+    },
+    async once(data) {
+      contentAnimation();
+    },
+  }]
+});
