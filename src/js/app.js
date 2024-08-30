075f9ba1c55c2e9261475f2c13e6494c2842e4fc
shahriyor-sharifjonov <shahriyordeveloping@gmail.com>
2024-08-30 12:21:42 +0500

    init

--- src/js/app.js
+++ src/js/app.js
@@ -190,7 +190,7 @@ import barba from '@barba/core';
 function pageTransition() {
   var tl = gsap.timeline();
   tl.to('ul.transition li', {duration: .5, scaleY: 1, transformOrigin: "bottom left", stagger: .2})
-  tl.to('ul.transition li', {duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .1})
+  tl.to('ul.transition li', {duration: .5, scaleY: 0, transformOrigin: "bottom left", stagger: .1, delay: .3})
 }
 
 function contentAnimation(){
